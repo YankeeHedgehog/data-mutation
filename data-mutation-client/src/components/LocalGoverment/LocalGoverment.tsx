7255d7b938db3dbbb45555556809bbac467470d9
yankee-hedgehog <yankee.hedgehog.610@gmail.com>
2024-07-07 13:49:44 +0900

    update: ReactRouter対応

---
+++ data-mutation-client/src/components/LocalGoverment/LocalGoverment.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function LocalGoverment() {
+  return <div>LocalGoverment</div>
+}
--- data-mutation-client/src/main.tsx
+++ data-mutation-client/src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import Router from './features/router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router />
+  </React.StrictMode>
 )
